0b45f31686437039f593b2ef378ec846acf0c5f9
Harikrishna241 <hkaranam241@gmail.com>
2026-03-05 10:01:56 -0800

    Create Content_sendemail.py

---
+++ Projects/Content_sendemail.py
@@ -0,0 +1,25 @@
+import requests
+import smtplib, ssl
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "hkaranam241@gmail.com"
+    password = "jlxl bxyr glgo waho"
+
+    receiver = "hkkaranam@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+api_key="5ee054ba41eb4a968f618f8e3eb82d60"
+url="https://newsapi.org/v2/everything?q=tesla&from=2026-02-05&sortBy=publishedAt&apiKey=5ee054ba41eb4a968f618f8e3eb82d60"
+reponse=requests.get(url)
+content=reponse.json()
+body=""
+for article in content["articles"]:
+    body=body+article["title"] + "\n" + article["description"] + 2*"\n"
+body=body.encode("utf-8")
+send_email(message=body)
